7e48d528565f6f8486298dea2f4499ce501820ca
MMp <mateusmoreirammp052@gmail.com>
2023-10-17 09:01:51 -0300

    roteiro 2

--- roteiro_2/app.py
+++ roteiro_2/app.py
@@ -3,5 +3,9 @@ import streamlit as st
 st.title("Roteiro 2: Widgets Interativos")
 
 value = st.slider("Escolha um valor", 0, 100)
+check = st.checkbox('Voce xekou?')
+togle = st.toggle('Voce togou?')
+radio = st.radio('Qual teu time',('vasco','mengo'))
+select = st.selectbox('Qual é melhor',('vasco','mengo'))
 if st.button("Mostrar valor do slider"):
-    st.write(f"Valor escolhido: {value}")
+    st.write(f"Valor escolhido: {value,check,togle,radio,select}")
