233ae1b0935bc75c2380fb1af4db4a92a3ad0dd6
MMp <mateusmoreirammp052@gmail.com>
2023-10-17 08:51:14 -0300

    roteiro_1 certo

--- roteiro_1/app.py
+++ roteiro_1/app.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.title("Roteiro 1: Primeiros Passos com Streamlit")
 
+input = st.text_input('Digite seu nome')
 if st.button("Clique-me"):
-    st.text_input('Digite seu nome')
+    st.write('nome:',input)
